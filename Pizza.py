b622ad5e51cb3054a6f65fc180de37366e663cea
Koreeeee <91974868+Koreeeee@users.noreply.github.com>
2023-05-29 16:01:47 +0200

    Add files via upload

---
+++ Pizza.py
@@ -0,0 +1,97 @@
+print("Assistant: Hello! Welcome to our pizza restaurant. What can i get you today?")
+
+
+question1 = input("User: ")
+
+
+if "pizza" in question1:
+    if "eggplant" in question1:
+        print(" Assistant: Great choice! Would you like a small, medium, or large eggplant pizza?")
+        question2 = input("User: ")
+        if "large" in question2:
+            if "coke" or "sprite" not in question2:
+                print("Assistant: Great choice! So that's a large eggplant pizza. Is that all for your order?")
+                question2 = input("User: ")
+            if "coke" in question2:
+                print("Assistant: Sure thing! So that's a large eggplant pizza and a coke.")
+            if "sprite" in question2:
+                print("Assistant: Sure thing! So that's a large eggplant pizza and a sprite.")
+                
+        if "medium" in question2:
+            if "coke" or "sprite" not in question2:
+                print("Assistant: Great choice! So that's a medium eggplant pizza. Is that all for your order?")
+                question2 = input("User: ")
+            if "coke" in question2:
+                print("Assistant: Sure thing! So that's a medium eggplant pizza and a coke.")
+            if "sprite" in question2:
+                print("Assistant: Sure thing! So that's a medium eggplant pizza and a sprite.") 
+                
+        if "small" in question2:
+            if "coke" or "sprite" not in question2:
+                print("Assistant: Great choice! So that's a small eggplant pizza. Is that all for your order?")
+                question2 = input("User: ")
+            if "coke" in question2:
+                print("Assistant: Sure thing! So that's a small eggplant pizza and a coke.")
+            if "sprite" in question2:
+                print("Assistant: Sure thing! So that's a small eggplant pizza and a sprite.")
+        
+    if "ham" in question1:
+        print(" Assistant: Great choice! Would you like a small, medium, or large ham pizza?")
+        question2 = input("User: ")
+        if "large" in question2:
+            if "coke" or "sprite" not in question2:
+                print("Assistant: Great choice! So that's a large ham pizza. Is that all for your order?")
+                question2 = input("User: ")
+            if "coke" in question2:
+                print("Assistant: Sure thing! So that's a large ham pizza and a coke.")
+            if "sprite" in question2:
+                print("Assistant: Sure thing! So that's a large ham pizza and a sprite.")
+                
+        if "medium" in question2:
+            if "coke" or "sprite" not in question2:
+                print("Assistant: Great choice! So that's a medium ham pizza. Is that all for your order?")
+                question2 = input("User: ")
+            if "coke" in question2:
+                print("Assistant: Sure thing! So that's a medium ham pizza and a coke.")
+            if "sprite" in question2:
+                print("Assistant: Sure thing! So that's a medium ham pizza and a sprite.") 
+                
+        if "small" in question2:
+            if "coke" or "sprite" not in question2:
+                print("Assistant: Great choice! So that's a small ham pizza. Is that all for your order?")
+                question2 = input("User: ")
+            if "coke" in question2:
+                print("Assistant: Sure thing! So that's a small ham pizza and a coke.")
+            if "sprite" in question2:
+                print("Assistant: Sure thing! So that's a small ham pizza and a sprite.")
+                
+    if "salami" in question1:
+        print(" Assistant: Great choice! Would you like a small, medium, or large salami pizza?")
+        question2 = input("User: ")
+        if "large" in question2:
+            if "coke" or "sprite" not in question2:
+                print("Assistant: Great choice! So that's a large salami pizza. Is that all for your order?")
+                question2 = input("User: ")
+            if "coke" in question2:
+                print("Assistant: Sure thing! So that's a large salami pizza and a coke.")
+            if "sprite" in question2:
+                print("Assistant: Sure thing! So that's a large salami pizza and a sprite.")
+                
+        if "medium" in question2:
+            if "coke" or "sprite" not in question2:
+                print("Assistant: Great choice! So that's a medium salami pizza. Is that all for your order?")
+                question2 = input("User: ")
+            if "coke" in question2:
+                print("Assistant: Sure thing! So that's a medium salami pizza and a coke.")
+            if "sprite" in question2:
+                print("Assistant: Sure thing! So that's a medium salami pizza and a sprite.") 
+                
+        if "small" in question2:
+            if "coke" or "sprite" not in question2:
+                print("Assistant: Great choice! So that's a small salami pizza. Is that all for your order?")
+                question2 = input("User: ")
+            if "coke" in question2:
+                print("Assistant: Sure thing! So that's a small salami pizza and a coke.")
+            if "sprite" in question2:
+                print("Assistant: Sure thing! So that's a small salami pizza and a sprite.")
+                
